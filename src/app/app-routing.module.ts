76d7f1bac471a5d018c2380a400132a977cc97b9
muhammadrafnasp <rafnasbasheer@gmail.com>
2022-12-19 19:28:52 +0530

    message

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,10 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { MainviewComponent } from './pages/mainview/mainview.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:'', component: MainviewComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,19 @@
+import { DragDropModule } from '@angular/cdk/drag-drop';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { MainviewComponent } from './pages/mainview/mainview.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MainviewComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    DragDropModule
   ],
   providers: [],
   bootstrap: [AppComponent]
